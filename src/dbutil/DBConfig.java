b1a475031a8f21227f701fa3c218315c3242a263
ayumitanaka <tanakayumi13@gmail.com>
2020-11-23 15:15:58 -0800

    commit

--- src/dbutil/DBConfig.java
+++ src/dbutil/DBConfig.java
@@ -4,7 +4,7 @@ public class DBConfig {
 
 	public static final String URL = "jdbc:mysql://localhost:3306/Recipe";
 	public static final String USER = "root";
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "zhpv3925";
 	public static final String DRIVER = "com.mysql.jdbc.Driver";
 	
 }
--- src/dbutil/DbHandler.java
+++ src/dbutil/DbHandler.java
@@ -2,6 +2,8 @@ package dbutil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DbHandler {
 	
@@ -29,5 +31,64 @@ public class DbHandler {
 			System.out.println(e.getMessage());
 		}
 	}
+	
+    public boolean insertUser(String firstName, String lastName, String email, String password){
+        PreparedStatement preparedStatement = null;
+        
+        try{
+            ResultSet resultSet;   
+            String countEmails = "SELECT COUNT(*) FROM USERS WHERE email = ?";
+            
+            preparedStatement = conn.prepareStatement(countEmails);
+            preparedStatement.setString(1, email);
+            
+            resultSet = preparedStatement.executeQuery();
+            
+            if(resultSet.next()){
+                if(resultSet.getInt(1) > 0){
+                    return false;
+                }
+            }
+            
+            String insertQuery = "INSERT INTO USERS (firstName, lastName, email, password) "
+                    + "VALUES (?, ?, ?, ?)";
+            
+            preparedStatement = conn.prepareStatement(insertQuery);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, password);
+            
+            int result = preparedStatement.executeUpdate();
+            return (result == 1);
+            
+        }catch(Exception e){
+            System.out.println("Insert user error: " + e.getMessage());
+        }
+        return false;
+    }
+    
+    public int checkCredentials(String email, String password){
+        String query = "SELECT id FROM USERS WHERE email = ? AND password = ?";
+        
+        PreparedStatement preparedStatement = null;
+        
+        try{
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            if(resultSet.next()){
+                return resultSet.getInt(1);
+            }
+            
+        }catch(Exception e){
+            System.out.println("Check credentials error, " + e.getMessage());
+        }
+        
+        return -1;
+    }
 
 }
