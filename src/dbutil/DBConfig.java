6886d3275ca443507eb180fa502a31d0f317076b
Daisugi <volble124@gmail.com>
2020-11-24 07:01:48 +0900

    modified dbhandler

--- src/dbutil/DBConfig.java
+++ src/dbutil/DBConfig.java
@@ -2,7 +2,7 @@ package dbutil;
 
 public class DBConfig {
 
-	public static final String URL = "jdbc:mysql://localhost:3306/recipe";
+	public static final String URL = "jdbc:mysql://localhost:3306/Recipe";
 	public static final String USER = "root";
 	public static final String PASSWORD = "";
 	public static final String DRIVER = "com.mysql.jdbc.Driver";
